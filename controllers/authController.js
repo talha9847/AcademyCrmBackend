d61b4c29f772ddc653deed27668b38ae808512f1
talha9847 <cse.210840131054@gmail.com>
2025-11-09 23:40:05 +0530

    talha

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,8 +13,8 @@ async function login(req, res) {
   const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "30d" });
   res.cookie("token", token, {
   httpOnly: true,
-  secure: false,
-  sameSite: "Lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
 });
 
