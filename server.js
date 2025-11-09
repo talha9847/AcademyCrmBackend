6572d4ad8adfebf024e29ec5fe6f68f1db05b7bb
talha9847 <cse.210840131054@gmail.com>
2025-11-09 23:57:30 +0530

    talha

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://academy-crm-frontend.vercel.app/",
+    origin: "https://academy-crm-frontend.vercel.app",
     credentials: true,
   })
 );
