e45186d44826c34cb1da5dd1589e69efe2c85a76
talha9847 <cse.210840131054@gmail.com>
2025-11-13 19:09:35 +0530

    today 2

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,14 +9,14 @@ async function login(req, res) {
   if (!user) {
     return res.status(401).json({ message: "Invalid Email or Password" });
   }
-  
+
   const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "30d" });
   res.cookie("token", token, {
-  httpOnly: true,
-  secure: true,
-  sameSite: "lax",
-  maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-});
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+  });
 
   return res.json({ role: user.role, name: user.fullName, email: user.email });
 }
@@ -42,12 +42,13 @@ async function checkAccess(req, res) {
 
 async function checkRoleAccess(req, res) {
   const { role } = req.body;
+  console.log(role);
   const user = await authMiddleware.foundClaims(req);
   if (!user) {
     return res.status(401).json({ message: "User not logged in" });
   }
   if (user.role != role) {
-    return res.status(403).json({ message: "Access denied" });
+    return res.status(403).json({ message: "Access denied", role: user.role });
   }
   return res.json({ message: "Access granted" });
 }
